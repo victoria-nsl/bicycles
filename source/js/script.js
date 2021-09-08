53f3033e0519245fafd6a1cd8a7be7e6a309af9c
victoria-nsl <vbnaish@yandex.ru>
2021-09-08 19:10:40 +0300

    Добавляет верстку шапки (навигация, мобильное меню)

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+const navigationMain = document.querySelector(".main-navigation");
+const navigationToggle = navigationMain.querySelector(".main-navigation__toggle");
+
+navigationMain.classList.remove("main-navigation--nojs");
+
+navigationToggle.addEventListener("click", () => {
+  if (navigationMain.classList.contains("main-navigation--closed")) {
+    navigationMain.classList.remove("main-navigation--closed");
+    navigationMain.classList.add("main-navigation--opened");
+    return;
+  }
+
+  navigationMain.classList.add("main-navigation--closed");
+  navigationMain.classList.remove("main-navigation--opened");
+});
