eaa5e12db61031b63587abbd64b3a13504af36b4
victoria-nsl <vbnaish@yandex.ru>
2021-09-08 21:05:58 +0300

    Изменяет настройки файла eslintrc

--- source/js/script.js
+++ source/js/script.js
@@ -1,16 +1,15 @@
-'use strict';
-const navigationMain = document.querySelector(".main-navigation");
-const navigationToggle = navigationMain.querySelector(".main-navigation__toggle");
+const navigationMain = document.querySelector('.main-navigation');
+const navigationToggle = navigationMain.querySelector('.main-navigation__toggle');
 
-navigationMain.classList.remove("main-navigation--nojs");
+navigationMain.classList.remove('main-navigation--nojs');
 
-navigationToggle.addEventListener("click", () => {
-  if (navigationMain.classList.contains("main-navigation--closed")) {
-    navigationMain.classList.remove("main-navigation--closed");
-    navigationMain.classList.add("main-navigation--opened");
+navigationToggle.addEventListener('click', () => {
+  if (navigationMain.classList.contains('main-navigation--closed')) {
+    navigationMain.classList.remove('main-navigation--closed');
+    navigationMain.classList.add('main-navigation--opened');
     return;
   }
 
-  navigationMain.classList.add("main-navigation--closed");
-  navigationMain.classList.remove("main-navigation--opened");
+  navigationMain.classList.add('main-navigation--closed');
+  navigationMain.classList.remove('main-navigation--opened');
 });
